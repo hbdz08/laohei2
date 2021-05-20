d03e9f0966c52d62059426083bf1239b9eae922d
lau <hbdz08@163.com>
2021-05-20 11:05:23 +0800

    test

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,6 +8,7 @@ const service = axios.create({
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
     'Content-Type': 'application/json; charset=utf-8',
+    'Access-Control-Allow-Origin': '*',
   },
 })
 export default service
