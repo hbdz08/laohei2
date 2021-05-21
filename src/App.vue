4a8ea559cd95113fffbbb3ca303394569da0f49e
lau <hbdz08@163.com>
2021-05-21 18:56:19 +0800

    修改标题

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     name: 'App',
     metaInfo: {
       title: 'App',
-      titleTemplate: '%s | Zero Pro',
+      titleTemplate: '老黑私服',
       htmlAttrs: { lang: 'en' },
       meta: [
         { charset: 'utf-8' },
